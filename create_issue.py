2c98599c6176b8b40149594b8090efef475d40de
kohlidevops <100069489+kohlidevops@users.noreply.github.com>
2023-12-30 15:58:00 +0530

    Update create_issue.py

--- create_issue.py
+++ create_issue.py
@@ -4,9 +4,9 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://your-domain.atlassian.net/rest/api/3/issue"
-
-auth = HTTPBasicAuth("email@example.com", "<api_token>")
+url = "https://slakshminarayanan.atlassian.net/rest/api/3/issue"
+API_TOKEN = "your-API-TOKEN-here"
+auth = HTTPBasicAuth("slakshminarayanan.6fs@gmail.com", API_TOKEN)
 
 headers = {
   "Accept": "application/json",
@@ -15,64 +15,12 @@ headers = {
 
 payload = json.dumps( {
   "fields": {
-    "assignee": {
-      "id": "5b109f2e9729b51b54dc274d"
-    },
-    "components": [
-      {
-        "id": "10000"
-      }
-    ],
-    "customfield_10000": "09/Jun/19",
-    "customfield_20000": "06/Jul/19 3:25 PM",
-    "customfield_30000": [
-      "10000",
-      "10002"
-    ],
-    "customfield_40000": {
-      "content": [
-        {
-          "content": [
-            {
-              "text": "Occurs on all orders",
-              "type": "text"
-            }
-          ],
-          "type": "paragraph"
-        }
-      ],
-      "type": "doc",
-      "version": 1
-    },
-    "customfield_50000": {
-      "content": [
-        {
-          "content": [
-            {
-              "text": "Could impact day-to-day work.",
-              "type": "text"
-            }
-          ],
-          "type": "paragraph"
-        }
-      ],
-      "type": "doc",
-      "version": 1
-    },
-    "customfield_60000": "jira-software-users",
-    "customfield_70000": [
-      "jira-administrators",
-      "jira-software-users"
-    ],
-    "customfield_80000": {
-      "value": "red"
-    },
     "description": {
       "content": [
         {
           "content": [
             {
-              "text": "Order entry fails when selecting supplier.",
+              "text": "My first JIRA ticket",
               "type": "text"
             }
           ],
@@ -82,59 +30,15 @@ payload = json.dumps( {
       "type": "doc",
       "version": 1
     },
-    "duedate": "2019-05-11",
-    "environment": {
-      "content": [
-        {
-          "content": [
-            {
-              "text": "UAT",
-              "type": "text"
-            }
-          ],
-          "type": "paragraph"
-        }
-      ],
-      "type": "doc",
-      "version": 1
-    },
-    "fixVersions": [
-      {
-        "id": "10001"
-      }
-    ],
     "issuetype": {
-      "id": "10000"
-    },
-    "labels": [
-      "bugfix",
-      "blitz_test"
-    ],
-    "parent": {
-      "key": "PROJ-123"
-    },
-    "priority": {
-      "id": "20000"
+      "id": "10005"
+      #Navigate to myapp - configure board - issues types - story - you can find the id in URL
     },
     "project": {
-      "id": "10000"
+      "key": "MYAP"
+      #This is the key of your myapp project
     },
-    "reporter": {
-      "id": "5b10a2844c20165700ede21g"
-    },
-    "security": {
-      "id": "10000"
-    },
-    "summary": "Main order flow broken",
-    "timetracking": {
-      "originalEstimate": "10",
-      "remainingEstimate": "5"
-    },
-    "versions": [
-      {
-        "id": "10000"
-      }
-    ]
+    "summary": "My first JIRA Ticket",
   },
   "update": {}
 } )
